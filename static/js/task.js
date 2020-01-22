f7cca22373d9433618e1667cdeaad845a91b1db3
galraz@gmail.com <galraz@gmail.com>
2020-01-22 17:02:49 -0500

    ‘asd’

--- static/js/task.js
+++ static/js/task.js
@@ -34,29 +34,7 @@ var instructions_block = {
 
 timeline.push(instructions_block);
 
-// get data from github file
-      var data2;
-      var msg = $.ajax({type: "GET",
-      url: "https://raw.githubusercontent.com/sradkani/CoCoSci/master/Experiment/sequences.csv",
-       async: false}).responseText;
 
-      console.log(Papa.parse(msg))
-      data2 = Papa.parse(msg)
-      data2 = data2['data']
-
-      var data2 = Object.values(data2);
-      console.log(Object.values(data2[0]).toString())
-
-      var test_stimuli = []
-    function csvValues(){
-      var arrayLength = data2.length;
-        for (var i = 0; i < arrayLength; i++) {
-          test_stimuli.push({stimulus: '<div style="font-size:45px;">' +
-          Object.values(data2[i]).toString().replace(/,/g, '  ') +
-          '</div>', data: {test_part: 'test'}})
-      }
-    }
-    csvValues();
 
 
 /* stimuli specifications */
